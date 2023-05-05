74800855d594f3dafc4baf26861c25bcf153e77d
rlp73 <99094429+rlp73@users.noreply.github.com>
2023-05-05 10:44:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const firstNameArray = [
     "Hyper", "Neo", "Terra", "Xeno", "Infinity", "Nano", "Quantum", "Omni",
     "CryptoGen", "Shadow", "Cyber", "Stellar", "Holo", "Meta", "Astra", "Epoch",
-    "Ether", "Nexus", "Para", "Synth", "Moon", "Unicorn", "HODL", "Whales",
+    "Ether", "Nexus", "Para", "Synth", "Moon", "Unicorn", "HODL",
     "Satoshis", "FOMO", "ToThe", "Bearish", "Bullish", "Hodlonaut", "Astro",
     "CryptoMancer", "BitRaptor", "Coinasaurus", "Blocktopus", "FUDster",
-    "Lambo", "ShillFactory", "Flippening", "CoinGuru"
+    "Lambo", "ShillFactory", "Flippening", "CoinGuru", "Moon", "Quantum", 
+    "Crypto", "Galaxy", "Ether", "Nexa", "Zeta", "Panda", "Alpha", 
+    "Byte", "Solar", "Flux", "Krypto", "Marble", "Phantom", "Saturn", "Mercury",
+    "Zenith", "Monarch", "Sparkle", "Wombo"
 ];
 
 const lastNameArray = [
     "Core", "Byte", "Flow", "Seed", "Trust", "Path", "Ring", "Zone", "Stack", "Warp",
     "Hype", "Quark", "Dime", "Flux", "Grid", "Crest", "Veil", "Arch", "Stride", "Roam",
     "Coin", "Chain", "Node", "Block", "Key", "Link", "Gem", "Pulse", "Hub", "Mint",
-    "Bolt", "Web", "Vault", "Bit", "Mesh", "Shard", "Code", "Fork", "Spark", "Wave"
+    "Bolt", "Web", "Vault", "Bit", "Mesh", "Shard", "Code", "Fork", "Spark", "Wave", 
+    "Dynamics", "King", "Core", "Gold", "Stream", "Loot", "Crunch", "Jumper", "Inu",
+    "Ledger", "Whale", "Vortex", "Phoenix"
 ];
 
 
